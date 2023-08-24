0cfaedbb13da7a3568232a754414ac76b7e6b315
Gohulan Somanathan <gohulan@outlook.com>
2023-08-24 12:15:33 +0530

    Add project files.

---
+++ SimphonyClientLogVerbosityIncreaser/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LogModifierApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("     SIMPHONY CLIENT LOG VERBOSITY INCREASER - #UTIL11 ");
+            Console.WriteLine("=========================================================  ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("     INCREASING LOG VERBOSITY LEVEL TO 100MB");
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("# DEVELOPER :- GOHULAN SOMANATHAN ");
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("# COMPANY   :- HOSPITALITY TECHNOLOGY");
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("# APP VER   :- 1.0.0 RELE 2023 AUGUST 24");
+            Console.WriteLine("=========================================================  ");
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            string logFilePath = "log.txt"; // Change this to your desired log file name
+
+            // Check if ServiceHost process is running and kill if necessary
+            KillServiceHostProcess();
+
+            // Copy and rename web.config file
+            string configFilePath = @"C:\Micros\Simphony\WebServer\wwwroot\EGateway\web.config.txt";
+            BackupAndModifyConfig(configFilePath);
+
+            // Add settings to the original web.config file
+            AddSettingsToConfig(configFilePath);
+
+            // Log Header 
+
+            string outputContent = @"
+=========================================================
+     SIMPHONY - CLIENT VERBOSITY INCREASER - #UTIL11
+=========================================================
+# MODIFIED DATE :- WEB.CONFIG.TXT - " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + @"
+=========================================================
+# DEVELOPER :- GOHULAN SOMANATHAN
+=========================================================
+# COMPANY   :- HOSPITALITY TECHNOLOGY
+=========================================================
+# APP VER   :- 1.0.0 RELE 2023 AUGUST 24
+=========================================================
+";
+
+            
+
+            // Log message
+
+            LogMessage(logFilePath, outputContent);
+            LogMessage(logFilePath, "Log verbosity level increased at " + Environment.MachineName);
+
+            // Start ServiceHost process
+            StartServiceHost();
+
+            // Exit the program
+            Environment.Exit(0);
+        }
+
+        static void KillServiceHostProcess()
+        {
+            Process[] processes = Process.GetProcessesByName("ServiceHost");
+            foreach (Process process in processes)
+            {
+                process.Kill();
+            }
+        }
+
+        static void BackupAndModifyConfig(string configFilePath)
+        {
+            string backupFilePath = configFilePath.Replace("web.config.txt", $"webconfig_{DateTime.Now:yyyyMMddHHmmss}.bak");
+            File.Copy(configFilePath, backupFilePath);
+
+            File.Copy(backupFilePath, configFilePath, true);
+        }
+
+        static void AddSettingsToConfig(string configFilePath)
+        {
+            string configContent = File.ReadAllText(configFilePath);
+            string newSettings = @"<add key=""LogArchiver.Enabled"" value=""true""/>
+    <add key=""LogArchiver.MinDiskUsedPercent"" value=""10""/>
+    <add key=""LogArchiver.MaxDiskUsedMB"" value=""100""/>
+    <add key=""LogArchiver.DaysToKeep"" value=""30""/>
+";
+            string modifiedConfigContent = configContent.Replace("</appSettings>", newSettings + "</appSettings>");
+            File.WriteAllText(configFilePath, modifiedConfigContent);
+        }
+
+        static void LogMessage(string logFilePath, string message)
+        {
+            using (StreamWriter writer = File.AppendText(logFilePath))
+            {
+                writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+            }
+        }
+
+        static void StartServiceHost()
+        {
+            Process.Start(@"C:\Micros\Simphony\WebServer\ServiceHost.exe");
+        }
+    }
+}
---
+++ SimphonyClientLogVerbosityIncreaser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimphonyClientLogVerbosityIncreaser")]
+[assembly: AssemblyDescription("Increase Simphony Client log verbosity level to 100MB")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CUBEMAK LABS (PVT) LTD")]
+[assembly: AssemblyProduct("SimphonyClientLogVerbosityIncreaser")]
+[assembly: AssemblyCopyright("Copyright ©  Somanathan Gohulan")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06d76221-89b6-42f5-9115-a68675d9c0cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
